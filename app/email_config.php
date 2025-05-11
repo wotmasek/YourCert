35915b3f655018dfda80aa96b8a10818b2cd1b41
Wojciech Masojć <wotmas09@gmail.com>
2025-05-11 21:20:57 +0200

    uploads empty folders fix

--- app/email_config.php
+++ app/email_config.php
@@ -3,9 +3,9 @@ require __DIR__ . '/../vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 
 return [
-    'host'        => '',
-    'username'    => '',
-    'password'    => '',
+    'host'        => 'smtp.gmail.com',
+    'username'    => 'TestYourCert@gmail.com',
+    'password'    => 'zlkr svan pqcn zvgy',
     'smtp_secure' => PHPMailer::ENCRYPTION_SMTPS,
     'smtp_auth'   => true,
     'port'        => 465,
